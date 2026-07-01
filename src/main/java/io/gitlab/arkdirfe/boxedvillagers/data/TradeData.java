2189a9bb1f7873245ffa8eeb47ee8ac62545628a
BigBrother <q1580836474@gmail.com>
2026-07-02 00:00:31 +0800

    fix: 修复新版本空槽位报错的问题

--- src/main/java/io/gitlab/arkdirfe/boxedvillagers/data/TradeData.java
+++ src/main/java/io/gitlab/arkdirfe/boxedvillagers/data/TradeData.java
@@ -8,12 +8,11 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-public class TradeData
-{
+public class TradeData {
     private final int reduction;
     private final int baseAmount;
     private final MerchantRecipe recipe;
-    
+
     /**
      * From MerchantRecipe
      *
@@ -21,65 +20,66 @@ public class TradeData
      * @param baseAmount Stack size of the first ingredient before reduction.
      * @param recipe     A merchant recipe representing the trade.
      */
-    public TradeData(final int reduction, final int baseAmount, @NotNull final MerchantRecipe recipe)
-    {
+    public TradeData(final int reduction, final int baseAmount, @NotNull final MerchantRecipe recipe) {
         this.reduction = reduction;
         this.baseAmount = baseAmount;
         this.recipe = recipe;
     }
-    
+
     /**
      * From NBTCompound
      *
      * @param recipeCompound The NBTCompound the trade is stored in.
      * @param cures          How often the villager has been cured.
      */
-    public TradeData(@NotNull final NBTCompound recipeCompound, final int cures)
-    {
+    public TradeData(@NotNull final NBTCompound recipeCompound, final int cures) {
         recipe = new MerchantRecipe(recipeCompound.getItemStack(Strings.TAG_OUTPUT), recipeCompound.getInteger(Strings.TAG_MAX_USES));
         reduction = recipeCompound.getInteger(Strings.TAG_REDUCTION);
         baseAmount = recipeCompound.getInteger(Strings.TAG_BASE_AMOUNT);
-        
+
         recipe.setMaxUses(recipeCompound.getInteger(Strings.TAG_MAX_USES));
         recipe.setUses(recipeCompound.getInteger(Strings.TAG_USES));
-        ItemStack i1 = recipeCompound.getItemStack(Strings.TAG_INPUT_1);
-        if (i1 != null) {
-            i1.setAmount(Math.max(baseAmount - reduction * cures, 1));
-            recipe.addIngredient(i1);
+        try {
+            ItemStack i1 = recipeCompound.getItemStack(Strings.TAG_INPUT_1);
+            if (i1 != null) {
+                i1.setAmount(Math.max(baseAmount - reduction * cures, 1));
+                recipe.addIngredient(i1);
+            }
+        } catch (Exception ignore) {
         }
-        ItemStack i2 = recipeCompound.getItemStack(Strings.TAG_INPUT_2);
-        if (i2 != null) {
-            recipe.addIngredient(i2);
+
+        try {
+            ItemStack i2 = recipeCompound.getItemStack(Strings.TAG_INPUT_2);
+            if (i2 != null) {
+                recipe.addIngredient(i2);
+            }
+        } catch (Exception ignore) {
         }
     }
-    
+
     // Getters
-    
-    public int getReduction()
-    {
+
+    public int getReduction() {
         return reduction;
     }
-    
-    public int getBaseAmount()
-    {
+
+    public int getBaseAmount() {
         return baseAmount;
     }
-    
+
     @NotNull
-    public MerchantRecipe getRecipe()
-    {
+    public MerchantRecipe getRecipe() {
         return recipe;
     }
-    
+
     // --- Serialization
-    
+
     /**
      * Adds a serialized representation to an NBTCompound.
      *
      * @param entry The compound to add to.
      */
-    public void serializeToNBT(@NotNull final NBTCompound entry)
-    {
+    public void serializeToNBT(@NotNull final NBTCompound entry) {
         List<ItemStack> ingredients = recipe.getIngredients();
         ItemStack i1 = !ingredients.isEmpty() ? ingredients.get(0) : null;
         ItemStack i2 = ingredients.size() > 1 ? ingredients.get(1) : null;
